3e88937d1126dd74f990e3d36870a5e4630c7917
fifth-island <113310477+fifth-island@users.noreply.github.com>
2022-12-03 13:55:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -57,13 +57,13 @@ http.createServer(async function (req, res) {
 		res.write ("The name is: " + pdata['user_input']);
 		user_value = pdata['user_input'];	
 		
-		
+		await connect_table();
+	  
+	  res.end();
 
 	});
 
-        await connect_table();
-	  
-	  res.end();
+        
 	  
 
   }
